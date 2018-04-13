2e5b347ab017ea16e7787714233a1189917ab015
Da, Annie <ada@lcs-mba3107.lcs.on.ca>
2018-04-13 10:20:00 -0400

    complete rotating letters

--- RotatingLetters/main.swift
+++ RotatingLetters/main.swift
@@ -8,30 +8,58 @@
 
 import Foundation
 
-//input
-
-//loop forever until we get good input
-while 1 == 1 {
+// INPUT
+// Loop forever until we get good input
+var validInput = "" // outside the loop -- we can use it later on AFTER the loop
+prompt: while 1 == 1 {
     
-    //prompt the user
-    print("input word is?")
+    // Prompt the user
+    print("Input word is? ")
     
-    //wait for input
-    //make sure it is not nil=
+    // Wait for input
+    // Test #1: Make sure it is not nil
     guard let givenInput = readLine() else {
         
-        //if we got here,input is nil
-        //prompt again
-        continue    //continue with the next iteration of while loop
+        // If we got here, input is nil
+        // Prompt again
+        continue    // continue with the next iteration of while loop
+        
     }
     
-    //test #2: Is the input the correct length
+    // Test #2: Is the input the correct length
     if givenInput.count < 1 || givenInput.count > 30 {
         
-        //if we got here, input is too short
+        // If we got here, input is too short or it's too long
         continue // prompt again
+        
     }
-
-    //if we got to this point, we know the input is good
-    break //puit the while loop and begin PROCESS section
+    
+    // Test #3: Check to ensure there is only UPPERCASE letters and no spaces
+    checking: for individualCharacter in givenInput {
+        
+        // DEBUG: Print the character we are looking at
+        print(individualCharacter)
+        
+        // Check the character
+        switch individualCharacter {
+        case "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z":
+            continue checking   // OK, go to iteration of loop that lets us
+        // inspect next character of the input string
+        default:
+            continue prompt     // Stop looking, we've found bad input, so go
+            // to next iteration of loop that lets prompt again
+            // for user input
+        }
+        
+    }
+    
+    // If we got to this point, we know the input is good
+    validInput = givenInput
+    break   // quit the while loop and begin PROCESS section
+    
 }
+
+// PROCESS
+// Now we have valid input that can be processed according to
+// the remaining requirements of the scenario
+print(validInput)
